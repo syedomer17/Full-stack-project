2cb36463706c0d4a781dddc886d4e9913541d756
syedomer17 <syedomerali2006@gmail.com>
2025-03-02 14:32:30 +0530

    signin signup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes,Route} from "react-router-dom"
-import Login from './pages/public/Login'
-import Register from './pages/public/Register'
+
 
 const App = () => {
   return (
     <>
     <Router>
       <Routes>
-        <Route path='/register' element ={<Register />}/>
+        
       </Routes>
     </Router>
     </>
